8bb1e4f7f9db01be64055a068f2fc0e8d3e0e9c5
Petia Davidova <petia_davidova@hotmail.com>
2018-03-11 10:10:56 +0000

    final version for HackTheBurgh
    Nothing works but we tried

--- click.py
+++ click.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2
 
 
-ref_pnts = []
+ref_pnts_sample = []
+ref_pnts_drawing = []
 
 def select_point(event,x,y,flags,param):
     global mouseX,mouseY
@@ -10,11 +11,16 @@ def select_point(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         print "Point selected: ", x, y
         mouseX,mouseY = x,y
-        ref_pnts.append([x,y])
-
+        cv2.circle(img2,(x,y),5,(100,100,100),-1)
+        ref_pnts_sample.append([x,y])
+    elif event == cv2.EVENT_RBUTTONDBLCLK:
+        print "Point selected: ", x, y
+        mouseX,mouseY = x,y
+        cv2.circle(img,(x,y),5,(100,100,100),-1)
+        ref_pnts_drawing.append([x,y])
 ###################################################
-ref_image_name = 'pica.png'
-image_name = 'drawing1-small.jpg'
+ref_image_name = 'pika.png'
+image_name = 'drawing-2.jpg'
 ref_img = cv2.imread(ref_image_name,cv2.IMREAD_GRAYSCALE)
 raw_img = cv2.imread(image_name,cv2.IMREAD_GRAYSCALE)
 
@@ -34,12 +40,23 @@ filtered = cv2.Laplacian(denoised,cv2.CV_64F)
 img=filtered
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',select_point)
+# cv2.resizeWindow('image', 600,600)
 
+img2=ref_img
+cv2.namedWindow('ref_image')
+cv2.setMouseCallback('ref_image',select_point)
+
+# vis = np.concatenate((img2, img), axis=1)
+# cv2.imshow('vis',vis)
 while(1):
-    cv2.imshow('image',img)
+    cv2.imshow('image', img)
+    cv2.imshow('ref_image',ref_img)
+
     k = cv2.waitKey(20) & 0xFF
     if k == 27:
         break
     elif k == ord('a'):
         print mouseX,mouseY
+        print ref_pnts_sample
+        print ref_pnts_drawing
         print "Bye.."
--- sift.py
+++ sift.py
@@ -1,10 +1,48 @@
-import cv2
+
 import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
-img = cv2.imread('pica.png')
-gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# img = cv2.imread('pica.png',0)
+#
+# # Initiate STAR detector
+# orb = cv2.ORB_create()
+# # find the keypoints with ORB
+# kp = orb.detect(img,None)
+#
+# # compute the descriptors with ORB
+# kp, des = orb.compute(img, kp)
+#
+# # draw only keypoints location,not size and orientation
+# img2 = cv2.drawKeypoints(img,kp,None, color=(0,255,0), flags=0)
+# plt.imshow(img2),plt.show()
 
-sift=cv2.SIFT()
-kp = sift.detect(gray,None)
-img=cv2.drawKeypoints(gray,kp)
-cv2.imwrite('sift_keypoints.jpg',img)
+
+
+
+
+img1 = cv2.imread('pika.png',0)          # queryImage
+img2 = cv2.imread('pika copy.png',cv2.IMREAD_GRAYSCALE) # trainImage
+
+blur = cv2.GaussianBlur(img2,(5,5),0)
+ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+img2 = th3
+# Initiate SIFT detector
+orb = cv2.ORB_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+
+# Match descriptors.
+matches = bf.match(des1,des2)
+
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+
+# Draw first 10 matches.
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:200], None, flags=2)
+
+plt.imshow(img3),plt.show()
